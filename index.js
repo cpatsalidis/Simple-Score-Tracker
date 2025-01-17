f5e27031082f96294381a300434ba482b8f7b2a1
cpatsalidis <christos.patsalidis20@imperial.ac.uk>
2025-01-17 16:20:13 +0200

    Added express, bootstrap, simple home page, partials, ejs

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+let port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/score', (req, res) => {
+    res.render('score');
+})
+
+app.get('*', (req, res) => {
+    res.send(`Not found`);
+})
+
+app.listen(port, () => {
+})
